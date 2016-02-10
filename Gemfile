5e0c0af6e3c9cb0ea9c741ab523c30db03e34235
c1505 <coreym247@gmail.com>
2016-02-10 18:13:36 +0000

    changes to deploy to heroku

--- Gemfile
+++ Gemfile
@@ -5,13 +5,20 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
+
 gem 'sqlite3'
+
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem "tux"
 gem 'rack-flash3'
+
+group :production do 
+  gem 'pg'
+  gem 'puma'
+end
 
 group :test do
   gem 'rspec'
--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,14 @@ require 'bundler/setup'
 require 'rack-flash'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do 
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
 
+configure :production do 
+  ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'] || 'postgres://localhost/mydb')
+end
 require_all 'app'
